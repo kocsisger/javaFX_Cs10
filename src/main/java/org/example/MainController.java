aee37ec5eca853ced63f4bde48dee2a80ce080c6
Kocsis Gergely <kocsisger@example.com>
2025-09-25 11:15:42 +0200

    Hello world

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -1,8 +1,18 @@
 package org.example;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 
 public class MainController {
+
+    @FXML
+    private Label seasonsLabel;
+
+    @FXML
+    void handleClick() {
+        System.out.println("hello world!!!");
+    }
+
     @FXML
     private void initialize() {
 
