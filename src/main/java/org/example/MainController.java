0dbb6c37175a9b4699c82384fad4cad32da38586
Kocsis Gergely <kocsisger@example.com>
2025-09-25 11:25:51 +0200

    1st Demo final

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -10,7 +10,11 @@ public class MainController {
 
     @FXML
     void handleClick() {
-        System.out.println("hello world!!!");
+        System.out.println("Hello world!!!");
+        if (seasonsLabel.getText().equals("Summer"))
+            seasonsLabel.setText("Autumn");
+        else
+            seasonsLabel.setText("Summer");
     }
 
     @FXML
