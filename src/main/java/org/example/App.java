4814b0a41cd71566c8754f410eb133bab72d847c
Kocsis Gergely <kocsisger@example.com>
2025-10-02 11:17:52 +0200

    MVC kész

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,14 +6,24 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.model.Model;
+import org.example.model.SubModel;
 
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+        //View
         var loader = new FXMLLoader(getClass().getResource("main.fxml"));
         var scene = new Scene(loader.load());
         scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+
+        //Model
+        Model model = new SubModel();
+
+        //Controller
+        MainController controller = (MainController) loader.getController();
+        controller.setModel(model);
 
         stage.setScene(scene);
         stage.setTitle("New Title");
---
+++ src/main/java/org/example/model/Model.java
@@ -0,0 +1,15 @@
+package org.example.model;
+
+import java.time.LocalDate;
+
+public class Model {
+    private Student student;
+
+    public Model() {
+        this.student = new Student(
+                "Robert Smith",
+                40,
+                LocalDate.of(2000,1, 1)
+        );
+    }
+}
